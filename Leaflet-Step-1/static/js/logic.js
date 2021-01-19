2fb34a001c433fc8dde3e4aa5673f1b2b6429cb5
Benjamin Kramskoi <ben.kramskoi@gmail.com>
2021-01-18 21:23:48 -0500

    last commit

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,1 +1,13 @@
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson"
+d3.json(queryUrl, function(data) {
+    function styleInfo(feature) {
+      return {
+        opacity: 1,
+        fillOpacity: 1,
+        fillColor: getColor(feature.properties.mag),
+        color: "#000000",
+        radius: getRadius(feature.properties.mag),
+        stroke: true,
+        weight: 0.5
+      };
+    }})
